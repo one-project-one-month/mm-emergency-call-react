b8de816e86e39cef33eac47fe01884f6fb75cf58
shinThantKyaw <secondgitacc@gmail.com>
2024-11-09 22:12:14 +0630

    feat/user-crud/table-page-ongoing-and-edit-form-done

--- mm-emergency-call-dashboard/src/app/(DashboardLayout)/users/page.tsx
+++ mm-emergency-call-dashboard/src/app/(DashboardLayout)/users/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import UserTable from "@/app/components/UserComponents/UserTable";
+import { Box, Button, Link, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
+import React from "react";
 
 const page = () => {
-    return (
-        <div>Users</div>
-    )
-}
+  return (
+    <Box>
+      <Typography>User</Typography>
 
-export default page
+      <Box
+        sx={{
+          mt: 4,
+          mb: 4,
+          width: "100%",
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
+        {" "}
+        <Link href={"/users/adding-user"}>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "#5D87FF",
+              color: "white",
+              ":hover": { bgcolor: "#253945", color: "#b0d8f0" },
+            }}
+          >
+            <AddIcon /> Add User
+          </Button>
+        </Link>
+      </Box>
+      <UserTable />
+    </Box>
+  );
+};
+
+export default page;
